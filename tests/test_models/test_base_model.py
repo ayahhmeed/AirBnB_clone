48bc177a36a62db0b42279ea241b0f25a0b65ff2
ayahhmeed <ayahhmeed@gmail.com>
2024-02-08 11:46:18 -0800

    tests

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+This is the unit tests for the class BaseModel
+"""
+
+import unittest
+import models
+from datetime import datetime
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase)
+"""
+Here is test cases for the BaseModel class
+"""
+
+def the_test_initialization(self):
+    """
+    Here to test the initialization of BaseModel class
+    """
+    the_model = BaseModel()
+    self.assertIsInstance(the_model.id, str)
+    self.assertIsInstance(the_model.created_at, datetime)
+    self.assertIsInstance(the_model.updated_at, datetime)
+
+    def test_string(self):
+        """
+        Here to test the string representaion of BaseModel intance
+        """
+        the_model = BaseModel()
+        self.assertEqual(str(the_model), "[BaseModel] ({}) {}".format(the_model.id, the_model.__dict__))
+
+        def save_test(self):
+            """
+            Here to test the save method of BaseModel instance
+            """
+            the_model = BaseModel()
+            the_old_updated = the_model.updated_at
+            the_model.save()
+            self.assertNotEqual(the_old_updated, the_model.updated.updated_at)
+
+            def dict_test(self):
+                """
+                Here is the test of to_dict method of BaseModel
+                """
+
+                the_model = BaseModel()
+                the_model_json = the_model.to_dict()
+                self.assertIsInstance(the_model_json, dist)
+                self.assertIn('__class__', the_model_json)
+                self.assertEqual(the_model_json['__class__'], 'BaseModel')
+                self.assertIn('updated_at', the_model_json)
+                self.assertIsInstance(the_model_json['created_at'], str)
+                self.assertIsInstance(the_model_json['updated_at'], str)
+
+                if __name__ == "__main__":
+                    unittest.main()
